1c420d45c6efd1c65b03a1f1d731f7652a8dfb7d
bstizzle <whereyouben@gmail.com>
2021-01-25 18:42:00 -0500

    gif overlays by region on map

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -2,14 +2,14 @@ import React, {useState, useEffect} from 'react'
 import WeatherDetailPopup from "./WeatherDetailPopup";
 import WeatherScroller from "./WeatherScroller";
 import Search from "./Search"
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, FeatureGroup, Rectangle, VideoOverlay, ImageOverlay } from 'react-leaflet'
 
 function MainPage({currentUser}) {
     const [weathers, setWeathers] = useState([])
     const [regions, setRegions] = useState([])
     const [selectedRegion, setSelectedRegion] = useState(1)
     const [clickedRegion, setClickedRegion] = useState(false)
-
+    
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_BASE_URL}/weathers`)
           .then(resp => resp.json())
@@ -26,8 +26,29 @@ function MainPage({currentUser}) {
         setClickedRegion(!clickedRegion)
     }
 
+    const featureGroups = regions.map((region) => {
+        const rectangle = [
+            [region["latMin"], region["longMin"]],
+            [region["latMax"], region["longMax"]]
+        ]
+
+        return(
+            <FeatureGroup>
+                <ImageOverlay url="https://media.giphy.com/media/3ohzdUimZF7zrY0fWo/giphy.gif" bounds={rectangle} opacity={0.6} play={false}/>
+                <Popup>
+                <div style={{width: '40%', height: '40%'}}>
+                    <WeatherDetailPopup allWeathers={weathers} selectedRegion={selectedRegion}/>
+                 </div>
+                </Popup>
+                <Rectangle bounds={rectangle}>
+                </Rectangle>
+            </FeatureGroup>
+        )
+    })
+
     const TheMap =  () => {
-        const position = [48.969404, -122.681376]
+        const position = [37.0902, - 95.7129]
+
         return (
            <MapContainer
             center={position}
@@ -38,12 +59,10 @@ function MainPage({currentUser}) {
                 url='https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
                 attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             />
+
+            {featureGroups}
+           
             <Marker position={position}>
-                <Popup>
-                    <div style={{width: '40%', height: '40%'}}>
-                        <WeatherDetailPopup allWeathers={weathers} selectedRegion={selectedRegion}/>
-                    </div>
-                </Popup>
             </Marker>
             </MapContainer>
         )
