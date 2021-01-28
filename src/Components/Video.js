8e1082483792464a50c8a8f758376b6a9e1f1ebb
bstizzle <whereyouben@gmail.com>
2021-01-28 11:01:08 -0500

    added repo-downloaded video and route for our login page

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import classes from "./BackgroundVideo.module.css"
+import video from "../loginBg.mp4"
 
 const Video = () => {
 
-    const bgVideo = "https://r4---sn-j5caxoxu-pn2s.googlevideo.com/videoplayback?expire=1611808383&ei=H-oRYI6jC5SDhgbFwY6gCg&ip=174.44.59.221&id=o-ADy0MsCFa9hvMDPKRRZbm6G5A4Mp__jWpMIF8sZPLAmU&itag=22&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&ns=b3LlTRf5ltKynuJ5SAdRUB0F&cnr=14&ratebypass=yes&dur=321.015&lmt=1568439406273668&fvip=4&c=WEB&txp=5532432&n=NepRS96tR9NTZbdjEK24&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIhAN5pzPLB4eGbwVQUYzzZoJM2fQtsBKdeZjd3KAiy3ORhAiAGQqGjHKwFskDOPGQPmga9MF7W3q5USSeUHR91eoVgXw%3D%3D&redirect_counter=1&cm2rm=sn-5aupjx0n5-pn2l7z&req_id=f22347d5b947a3ee&cms_redirect=yes&mh=jV&mm=29&mn=sn-j5caxoxu-pn2s&ms=rdu&mt=1611786768&mv=m&mvi=4&pcm2cms=yes&pl=21&lsparams=mh,mm,mn,ms,mv,mvi,pcm2cms,pl&lsig=AG3C_xAwRgIhAMOe1aJkyPKUtqmHUU5qFtVbt3KuAaOoEFCa-vc8dk7OAiEA5vOKovRSoPk0MbuPh17T1ZsmpFBHd-HGY6DpNZeOIU0%3D"
-    
+    const bgVideo = video
+    // https://r4---sn-5aupjx0n5-pn2l.googlevideo.com/videoplayback?expire=1611864469&ei=NcUSYLr4I4LP8wSXlqAQ&ip=174.44.59.221&id=o-AAeZvh2L7D02saG7dd-z8Y2G7EIr0_tnQeL-h4NaBOAt&itag=22&source=youtube&requiressl=yes&mh=jV&mm=31%2C29&mn=sn-5aupjx0n5-pn2l%2Csn-j5caxoxu-pn2s&ms=au%2Crdu&mv=m&mvi=4&pl=21&initcwndbps=1570000&vprv=1&mime=video%2Fmp4&ns=VwD-SBMski6o5YVVDVydym0F&cnr=14&ratebypass=yes&dur=321.015&lmt=1568439406273668&mt=1611842438&fvip=4&c=WEB&txp=5532432&n=GGjcIp8EfZEKbsuQHesU&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRAIgTFincsW4AGVcQdgLgDlfZYvsYLdOgh2nHGs2o0cQ_4YCIAGE7AJjIOBzAET5PdHe_SRyXkykxp8prazsauieq1SO&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIgdPsrGMjeKTjyG-kPCwtludXW9_Nv_-pmlkHV12yF0IgCIQDEmuuf87le3BWA3WR-BTa98yiZ-5EuPFy_W7z-QzxAfw%3D%3D
     return(
         <div className={classes.Container}>
             <video autoPlay="autoplay" loop="loop" muted className={classes.Video} >
